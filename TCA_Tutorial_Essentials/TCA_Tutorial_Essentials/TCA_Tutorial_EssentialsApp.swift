d6276f4a501522279ead2f8fa35614548b86f8f1
kyunghoonKim <kimkyunghun3@naver.com>
2023-08-14 10:35:16 +0900

    fix: 깃헙 폴더 안눌리는 현상 해결

---
+++ TCA_Tutorial_Essentials/TCA_Tutorial_Essentials/TCA_Tutorial_EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Tutorial_EssentialsApp.swift
+//  TCA_Tutorial_Essentials
+//
+//  Created by LS-MAC-00211 on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Tutorial_EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA_Tutorial_Navigation/TCA_Tutorial_Navigation/TCA_Tutorial_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Tutorial_NavigationApp.swift
+//  TCA_Tutorial_Navigation
+//
+//  Created by LS-MAC-00211 on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Tutorial_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
