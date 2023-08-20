21026bbc03687a742877d65c153736d33fa7d6a6
kyunghoonKim <kimkyunghun3@naver.com>
2023-08-21 08:16:18 +0900

    LandMark Part1, Creating and Combining Views

---
+++ LandMark_Tutorial/LandMark_Tutorial/LandMark_Tutorial/LandMark_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMark_TutorialApp.swift
+//  LandMark_Tutorial
+//
+//  Created by LS-MAC-00211 on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct LandMark_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
