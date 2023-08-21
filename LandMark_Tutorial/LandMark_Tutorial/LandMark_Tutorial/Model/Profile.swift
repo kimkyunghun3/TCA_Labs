bacdf0530f7fed8acfc375d7af66c49374557792
kyunghoonKim <kimkyunghun3@naver.com>
2023-08-21 14:30:55 +0900

    LandMark Part 7, Working with UI Controls

---
+++ LandMark_Tutorial/LandMark_Tutorial/LandMark_Tutorial/Model/Profile.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌹"
+        case summer = "🌞"
+        case autumn = "🌰"
+        case winter = "⛄️"
+        
+        var id: String { rawValue }
+    }
+}
