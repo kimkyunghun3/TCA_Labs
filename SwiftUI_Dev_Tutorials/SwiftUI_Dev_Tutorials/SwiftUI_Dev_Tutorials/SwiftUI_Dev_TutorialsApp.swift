76e5e62f6bf288572cabe2fd55a9f29521a82b22
kyunghoonKim <kimkyunghun3@naver.com>
2023-08-24 14:14:34 +0900

    SwiftUI Dev Tutorial Part 1

---
+++ SwiftUI_Dev_Tutorials/SwiftUI_Dev_Tutorials/SwiftUI_Dev_Tutorials/SwiftUI_Dev_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Dev_TutorialsApp.swift
+//  SwiftUI_Dev_Tutorials
+//
+//  Created by LS-MAC-00211 on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Dev_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
