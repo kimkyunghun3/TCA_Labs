bc67a02ae62fc8e32edea4177744930620b27981
kyunghoonKim <kimkyunghun3@naver.com>
2023-08-30 13:19:15 +0900

    과제 3 계정 View 레이아웃 및 카드 뷰 추가

--- Payco_Clone/Payco_Clone/AccountView.swift
+++ Payco_Clone/Payco_Clone/AccountView.swift
@@ -2,48 +2,72 @@ import SwiftUI
 
 struct AccountView: View {
   var body: some View {
+    ZStack {
+      RoundedRectangle(cornerRadius: 30)
+        .frame(height: 200)
+        .foregroundColor(Color.red)
+        .padding()
       
       VStack(alignment: .leading) {
         Text("PAYCO Point")
-          .padding(.top, 30)
           .foregroundColor(.white)
-          .font(.caption2)
+          .font(.body)
           .bold()
-        
+          .padding(.vertical, 5)
         
         Button {
           // Button Detail View 이동
         } label: {
-          Text("0 P >")
-            .font(.title2)
+          Text("309 P >")
+            .font(.title)
             .foregroundColor(.white)
             .bold()
         }
-        .padding(.top, 20)
-        
-        Spacer()
+        .padding(.bottom, 30)
         
         Button {
           // 계좌 등록 View 이동
         } label: {
-          Text("충전 계좌를 등록해주세요.")
-            .font(.caption2)
+          Text("토스뱅크 (9920)")
+            .font(.caption)
             .foregroundColor(.yellow)
             .underline()
         }
-        .padding(.bottom, 30)
+        .padding(.bottom, 20)
       }
-      .padding(.leading, 20)
-      .frame(width: 400, height: 200, alignment: .leading)
-      .background(Color.purple)
-      .cornerRadius(20)
-      .padding()
+      .padding(.trailing, 250)
+      
+      VStack {
+        Image("Payco_Card")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 150, height: 150)
+          .shadow(radius: 20, x: 15, y: 15)
+
+        
+        Button {
+          
+        } label: {
+          Text("카드관리")
+            .font(.subheadline)
+        }
+        
+        .frame(width: 80, height: 40)
+        .foregroundColor(.white.opacity(0.8))
+        .background(Color.white.opacity(0.3))
+        .cornerRadius(15)
+        .padding(.leading, 50)
+      }
+      .padding(.leading, 200)
+      .padding(.bottom, 50)
+ 
+    }
   }
 }
 
 struct AccountView_Previews: PreviewProvider {
   static var previews: some View {
     AccountView()
-      .previewLayout(.sizeThatFits)
+    //      .previewLayout(.sizeThatFits)
   }
 }
