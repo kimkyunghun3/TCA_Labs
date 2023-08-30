2c76e44948ba46f2a0fac1cba2f765b771a71647
kyunghoonKim <kimkyunghun3@naver.com>
2023-08-31 08:27:26 +0900

    과제3 카드 신청하기 뷰 무한스크롤 구현

--- Payco_Clone/Payco_Clone/CardRegisterView.swift
+++ Payco_Clone/Payco_Clone/CardRegisterView.swift
@@ -7,7 +7,7 @@ struct PagingControls: View {
 
   var body: some View {
     HStack {
-      ForEach(0..<totalCount, id: \.self) { pagingIndex in
+      ForEach(1..<totalCount + 1, id: \.self) { pagingIndex in
         RoundedRectangle(cornerRadius: 10)
           .frame(width: pagecontrolTracker == pagingIndex ? 40 : 6, height: 6)
           .foregroundColor(pagecontrolTracker == pagingIndex ? .black : .gray.opacity(0.2))
@@ -19,20 +19,24 @@ struct PagingControls: View {
 
 struct CardRegisterView: View {
   private let titles: [String] = [
+    "내 주변 가맹점 확인하고\n주요 브랜드에서 적립받기",
     "결제할 때 사용 가능한\n5천 포인트 바로 받기",
     "PAYCO 포인트로\n해외결제도 된다는 사실",
     "이달의 브랜드 확인하고\n더 많은 포인트를 적립 받으세요",
-    "내 주변 가맹점 확인하고\n주요 브랜드에서 적립받기"
+    "내 주변 가맹점 확인하고\n주요 브랜드에서 적립받기",
+    "결제할 때 사용 가능한\n5천 포인트 바로 받기"
   ]
   
   private let descriptions: [String] = [
+    "패션, 뷰티 브랜드부터\n집 주변 카페까지!",
     "신청까지 최대 1분,\n신청완료 즉시 5천P 지급!",
     "해외 호텔 & 항공 예약 시\n할인은 기본,\n현지화 출금까지 가능해요",
     "결제할 때마다\n최대 15%까지 적립!",
-    "패션, 뷰티 브랜드부터\n집 주변 카페까지!"
+    "패션, 뷰티 브랜드부터\n집 주변 카페까지!",
+    "신청까지 최대 1분,\n신청완료 즉시 5천P 지급!"
   ]
   
-  @State var pagecontrolTracker: Int  = 0
+  @State var pagecontrolTracker: Int  = 1
   
   var body: some View {
     VStack {
@@ -45,13 +49,18 @@ struct CardRegisterView: View {
           .padding(.horizontal)
             .tag(index)
         }
-        
+      }
+      .onChange(of: pagecontrolTracker) { _ in
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+          if pagecontrolTracker == 5 { pagecontrolTracker = 1 }
+          if pagecontrolTracker == 0 { pagecontrolTracker = 4 }
+        }
       }
       .tabViewStyle(.page(indexDisplayMode: .never))
       
       PagingControls(
         pagecontrolTracker: $pagecontrolTracker,
-        totalCount: titles.count
+        totalCount: 4
       )
       .offset(y: -20)
     }
