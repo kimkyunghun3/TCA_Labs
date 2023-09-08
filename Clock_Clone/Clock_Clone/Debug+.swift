63e0468ee44ff9459e0c47a7adc08a2f8e7d651f
kyunghoonKim <kimkyunghun3@naver.com>
2023-09-08 15:51:07 +0900

    과제4 디버깅 코드 추가

---
+++ Clock_Clone/Clock_Clone/Debug+.swift
@@ -0,0 +1,35 @@
+import ComposableArchitecture
+import SwiftUI
+
+struct FrameDebugModifier: ViewModifier {
+
+  let color: Color
+
+  func body(content: Content) -> some View {
+    content
+    #if DEBUG
+      .overlay(GeometryReader(content: overlay(for:)))
+    #endif
+  }
+
+  private func overlay(for geometry: GeometryProxy) -> some View {
+    ZStack(alignment: .topTrailing) {
+      Rectangle()
+        .strokeBorder(style: .init(lineWidth: 1, dash: [3]))
+        .foregroundColor(color)
+
+      Text("(\(Int(geometry.frame(in: .global).origin.x)), \(Int(geometry.frame(in: .global).origin.y))) \(Int(geometry.size.width))x\(Int(geometry.size.height))")
+        .font(.caption2)
+        .minimumScaleFactor(0.5)
+        .foregroundColor(color)
+        .padding(3)
+        .offset(y: -20)
+    }
+  }
+}
+
+extension View {
+  func debug(_ color: Color = .red) -> some View {
+    modifier(FrameDebugModifier(color: color))
+  }
+}
