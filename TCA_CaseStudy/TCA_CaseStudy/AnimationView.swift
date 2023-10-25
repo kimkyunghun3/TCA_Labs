147272f159bdaf82d106609831c82f0c8584602e
kyunghoonKim <kimkyunghun3@naver.com>
2023-10-25 09:51:35 +0900

    feat: AnimationView 완성

--- TCA_CaseStudy/TCA_CaseStudy/AnimationView.swift
+++ TCA_CaseStudy/TCA_CaseStudy/AnimationView.swift
@@ -4,26 +4,41 @@ import ComposableArchitecture
 struct AnimationCore: Reducer {
   struct State: Equatable {
     @PresentationState var alert: AlertState<Action.Alert>?
+    var isOn: Bool = false
+    var circleColor = Color.black
+    var circlePoint: CGPoint?
   }
   enum Action: Equatable {
     case alert(PresentationAction<Alert>)
-    case bigModeTapped
+    case bigModeTapped(Bool)
     case rainbowButtonTapped
     case resetButtonTapped
+    case setColor(Color)
+    case tapped(CGPoint)
     
     enum Alert: Equatable {
-      case cancelButtonTapped
       case resetButtonTapped
     }
   }
   
+  @Dependency(\.continuousClock) var clock
+  
+  private enum CancelList { case rainbow }
+  
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
-      case .bigModeTapped:
+      case let .bigModeTapped(isOn):
+        state.isOn = isOn
         return .none
       case .rainbowButtonTapped:
-        return .none
+        return .run { send in
+          for color in [Color.red, .blue, .green, .orange, .pink, .purple, .yellow, .blue] {
+            await send(.setColor(color), animation: .linear)
+            try await self.clock.sleep(for: .seconds(1))
+          }
+        }
+        .cancellable(id: CancelList.rainbow)
       case .resetButtonTapped:
         state.alert = AlertState {
           TextState("Reset state?")
@@ -35,11 +50,17 @@ struct AnimationCore: Reducer {
             TextState("Reset")
           }
         }
-        
         return .none
       case .alert(.presented(.resetButtonTapped)):
-        return .none
+        state = State()
+        return .cancel(id: CancelList.rainbow)
       case .alert:
+        return .none
+      case let .setColor(color):
+        state.circleColor = color
+        return .none
+      case let .tapped(point):
+        state.circlePoint = point
         return .none
       }
     }
@@ -56,7 +77,7 @@ struct AnimationView: View {
     self.viewStore = ViewStore(self.store, observe: { $0 } )
   }
   var body: some View {
-    VStack {
+    VStack(alignment: .leading) {
       Text("""
   This screen demonstrates how changes to application state can drive animations. Because the \
   `Store` processes actions sent to it synchronously you can typically perform animations \
@@ -73,22 +94,51 @@ struct AnimationView: View {
   Try it out by tapping or dragging anywhere on the screen to move the dot, and by flipping the \
   toggle at the bottom of the screen.
   """)
-      
-      HStack {
-        Text("Big mode")
-        
+      .gesture(
+        DragGesture(minimumDistance: 0).onChanged { gesture in
+          viewStore.send(
+            .tapped(
+              gesture.location),
+            animation: .interactiveSpring(
+              response: 0.25,
+              dampingFraction: 0.1)
+          )
+        }
+      )
+      .overlay {
+        GeometryReader(content: { geometry in
+          Circle()
+            .fill(viewStore.circleColor)
+            .colorInvert()
+            .blendMode(.difference)
+            .frame(width: 50, height: 50)
+            .scaleEffect(viewStore.isOn ? 2 : 1)
+            .position(
+              x: viewStore.circlePoint?.x ?? geometry.size.width / 2 ,
+              y: viewStore.circlePoint?.y ?? geometry.size.height / 2
+            )
+            .offset(y: viewStore.circlePoint == nil ? 0 : -44)
+        })
+        .allowsHitTesting(false)
       }
+      
+      Toggle(
+        "Big mode",
+        isOn:
+          viewStore
+          .binding(get: \.isOn, send: { .bigModeTapped($0) })
+          .animation(.interactiveSpring(response: 0.25, dampingFraction: 0.1))
+      )
       
       Button("Rainbow") {
         viewStore.send(.rainbowButtonTapped)
       }
-      
       Button("Reset") {
         viewStore.send(.resetButtonTapped)
       }
     }
-    .alert(store: self.store.scope(state: \.$alert, action: { .alert($0) }))
     .padding()
+    .alert(store: self.store.scope(state: \.$alert, action: { .alert($0) }))
   }
 }
 
